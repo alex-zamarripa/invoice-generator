754d3dc2f806c0c0c9b2e8a5b4cb5c70ce1873f1
Alex Zamarripa <AriyairAlejandroZV@gmail.com>
2023-05-07 12:00:38 -0600

    deploy config changed

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo "Deploying to GitHub Pages..."
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git remote set-url origin "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}"
+git remote set-url origin "https://${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}"
 git fetch
 git checkout -b gh-pages origin/gh-pages || git checkout --orphan gh-pages
 rm -rf ./*
